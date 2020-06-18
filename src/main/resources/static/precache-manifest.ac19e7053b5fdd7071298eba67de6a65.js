bf944fd754884e7597f12229f96b912d0f54124c
qamar929 <52096570+qamar929@users.noreply.github.com>
2020-06-18 19:18:38 +0500

    Module 2

---
+++ src/main/resources/static/precache-manifest.ac19e7053b5fdd7071298eba67de6a65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca358be7592737a1e10c8952b7351dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fce1520caafad9b4e0d",
+    "url": "/static/css/2.6d92adba.chunk.css"
+  },
+  {
+    "revision": "bc141f10d88b0aa9d05b",
+    "url": "/static/css/main.365595db.chunk.css"
+  },
+  {
+    "revision": "5fce1520caafad9b4e0d",
+    "url": "/static/js/2.12cba99b.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.12cba99b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc141f10d88b0aa9d05b",
+    "url": "/static/js/main.20756b99.chunk.js"
+  },
+  {
+    "revision": "67985ecc74a273f5f703",
+    "url": "/static/js/runtime-main.27286a68.js"
+  },
+  {
+    "revision": "01c57730f3e4521d43e9371ea21e6094",
+    "url": "/static/media/back.01c57730.jpg"
+  }
+]);
